779b18a059b5c1214955a59b2b6674d0bc15430f
Jebumon <jebumon@gmail.com>
2022-10-24 01:05:03 +0100

    Created different appsettings.json  files for various environments

--- BookManagerApi/Program.cs
+++ BookManagerApi/Program.cs
@@ -11,8 +11,25 @@ builder.Services.AddScoped<IBookManagementService, BookManagementService>();
 builder.Services.AddControllers();
 var connectionString = builder.Configuration.GetConnectionString("BookManagerApi");
 /*builder.Services.AddDbContext<BookContext>(option =>
-    option.UseInMemoryDatabase("BookDb"));*/
-builder.Services.AddDbContext<BookContext>(option => option.UseMySql(connectionString,ServerVersion.AutoDetect(connectionString)));
+    option.UseInMemoryDatabase("BookDb"));
+builder.Services.AddDbContext<BookContext>(option => option.UseMySql(connectionString,ServerVersion.AutoDetect(connectionString)));*/
+
+if (builder.Environment.EnvironmentName == "Testing")
+{
+    // in test environment use a fresh in-memory DB
+    builder.Services.AddDbContext<BookContext>(option =>
+    option.UseInMemoryDatabase("BookDb"));
+}
+else
+{
+    // connect to the local MySQL dev database
+    builder.Services.AddDbContext<BookContext>(option =>
+
+    option.UseMySql(connectionString,
+
+    ServerVersion.AutoDetect(connectionString)));
+}
+
 
 // Configure Swagger/OpenAPI Documentation
 // You can learn more on this link: https://aka.ms/aspnetcore/swashbuckle
@@ -22,7 +39,8 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() ||
+    app.Environment.EnvironmentName == "Testing")
 {
     app.UseSwagger();
     app.UseSwaggerUI();
